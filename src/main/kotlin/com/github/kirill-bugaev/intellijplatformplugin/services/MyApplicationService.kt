b4c5580734a0a46359a42a51b8b167125d6f206d
GitHub Action <action@github.com>
2020-07-01 08:38:11 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/kirill-bugaev/intellijplatformplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.kirill-bugaev.intellijplatformplugin.services
+
+import com.github.kirill-bugaev.intellijplatformplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
